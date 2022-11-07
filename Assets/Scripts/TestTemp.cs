2f08aee3d9af3aa4ff1a6905509e6d43cb95741c
KubisBarb <82451125+KubisBarb@users.noreply.github.com>
2022-11-07 19:03:44 +0100

    Room blockout add
    added fbx to main scene

---
+++ Assets/Scripts/TestTemp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTemp : MonoBehaviour
+{
+    GameObject[] foundItems;
+    GameObject[] notFoundItems;
+
+    void GetLoot(float skill2, float skill3)
+    {
+        //is new or not
+
+    }
+}
